b92d4028a069a75401b838490351d2355f04facd
Ivan Naymushin <ivan4bounty@gmail.com>
2020-08-13 12:36:53 +0500

    Add psycopg2==2.8.5, updated Wheel & SetupTools, and connected local pstgresql

--- stp_helper/settings.py
+++ stp_helper/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'stp_helper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': 'Vancho13',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
