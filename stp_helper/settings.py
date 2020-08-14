86e927dd9f78562c5d122a92f2fb5f13aa21a365
Ivan Naymushin <ivan4bounty@gmail.com>
2020-08-14 12:27:58 +0500

    Before deploy changes to heroku

--- stp_helper/settings.py
+++ stp_helper/settings.py
@@ -77,24 +77,28 @@ WSGI_APPLICATION = 'stp_helper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo_1',
-        'USER': 'ivan',
-        'PASSWORD': 'Vancho13',
-        'HOST': 'database-1.cpvwfdebxnfm.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# # AWS Settings
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'demo_1',
+#         'USER': 'ivan',
+#         'PASSWORD': 'Vancho13',
+#         'HOST': 'database-1.cpvwfdebxnfm.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
